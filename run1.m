2f15e131d5887606e71eceda4027fd72a05e34fd
Frantzen Christian <frantzenchristian@hotmail.com>
2017-12-12 19:54:41 +0000

    adding formulas for normalization

--- run1.m
+++ run1.m
@@ -5,7 +5,6 @@ imgCell={trainImg};
 imgCellLen=length(imgCell);
 resizeImageDim=16;
 imgVector1=zeros(100,resizeImageDim*resizeImageDim);
-%tinyFeature=zeros(1500,resizeImageDim*resizeImageDim);
 tinyFeature=double.empty;
 for ii=1:15
     for jj=1:100
@@ -21,11 +20,18 @@ for ii=1:15
         imgResize=imresize(imgRead,[resizeImageDim resizeImageDim]);
         imgResize=im2double(imgResize);
         imgVector = reshape(imgResize,[resizeImageDim*resizeImageDim,1]);
-        imgVector=imgVector-mean(imgVector);
-        imgVector = imgVector/std(imgVector);
+        %imgVector=imgVector-mean(imgVector);
+        %imgVector = imgVector/std(imgVector);
         imgVector1(jj,:) = imgVector;
     end
     tinyFeature=vertcat(tinyFeature,imgVector1);
 end
 
-
+% %%
+% IM=imgResize;
+% IM=IM-mean(IM);
+% IM=IM/norm(IM);
+% %%
+% IM=IM.*IM;
+% s=sum(sum(IM));
+% disp(sqrt(s));
