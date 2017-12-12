4017e49d75aa181ac7dd45fbbd5eef79bcba951f
Frantzen Christian <frantzenchristian@hotmail.com>
2017-12-12 18:04:28 +0000

    in progress

--- run1.m
+++ run1.m
@@ -1,19 +1,43 @@
 clc;
 clear all;
-trainImg=imageSet('C:\Users\aswin\computerVisionClassifiers\training\training','recursive');
+trainImg=imageSet('.\training','recursive');
 imgCell={trainImg};
 imgCellLen=length(imgCell);
 imgVector1=zeros(100,256);
-tinyFeature=zeros(1500,3840);
+tinyFeature=zeros(1500,256);
 for ii=1:15
     for jj=1:100
-    imgRead=read(trainImg(ii),jj);
-    imgResize=imresize(imgRead,[16 16]);
-    imgVector = imgResize(:)'; 
-    imgVector1(jj,:) = imgVector;
+        if jj == 1
+            disp(ii);
+        end
+        imgRead=read(trainImg(ii),jj);
+        imgRead=im2double(imgRead);
+        [xDim,yDim]=size(imgRead);
+        if xDim~=yDim
+            %disp(ii+", "+jj);
+            xCenter=floor(xDim/2);
+            yCenter=floor(yDim/2);
+            squareDim=yCenter;
+            if yCenter>xCenter
+                squareDim = xCenter;
+            else
+                squareDim = yCenter;
+            end
+            xLeft=xCenter-squareDim;
+            width=squareDim*2;%xLeft+2*xLim;
+            yTop=yCenter-squareDim;
+            height=squareDim*2%yTop+2*yLim;
+            imgRead2=imcrop(imgRead, [xLeft, yTop, width, height]);
+            imgRead=imcrop(imgRead, [xLeft, yTop, width, height]);
+        end
+        imgResize=imresize(imgRead,[16 16]);
+        imgVector = reshape(imgResize,[256,1]);
+        imgVector=imgVector-mean(imgVector);
+        imgVector = imgVector/std(imgVector);
+        imgVector1(jj,:) = imgVector;
     end
     tinyFeature=vertcat(tinyFeature,imgVector1);
-    jj=0;
+    %jj=0;
 end
 
 
