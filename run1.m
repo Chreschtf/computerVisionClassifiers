3e428a80d62caf67ac4401874e8b11c2adf7a80a
AswinkarthikeyenAK <33458020+AswinkarthikeyenAK@users.noreply.github.com>
2017-12-12 14:34:47 +0000

    Add files via upload

--- run1.m
+++ run1.m
@@ -1,13 +1,19 @@
+clc;
+clear all;
 trainImg=imageSet('C:\Users\aswin\computerVisionClassifiers\training\training','recursive');
-trainsetLeng=length(trainImg);
+imgCell={trainImg};
+imgCellLen=length(imgCell);
 imgVector1=zeros(100,256);
-for ii=1:trainsetLeng
+tinyFeature=zeros(1500,3840);
+for ii=1:15
     for jj=1:100
     imgRead=read(trainImg(ii),jj);
     imgResize=imresize(imgRead,[16 16]);
     imgVector = imgResize(:)'; 
     imgVector1(jj,:) = imgVector;
     end
+    tinyFeature=vertcat(tinyFeature,imgVector1);
+    jj=0;
 end
 
 
