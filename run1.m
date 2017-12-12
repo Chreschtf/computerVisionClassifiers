da97b2dadca16fa86f99f89b84d05165643e8d76
AswinkarthikeyenAK <33458020+AswinkarthikeyenAK@users.noreply.github.com>
2017-12-12 12:23:06 +0000

    Add files via upload

--- run1.m
+++ run1.m
@@ -1,10 +1,10 @@
 trainImg=imageSet('C:\Users\aswin\computerVisionClassifiers\training\training','recursive');
 trainsetLeng=length(trainImg);
-
 for ii=1:trainsetLeng
     for jj=1:100
     imgRead=read(trainImg(ii),jj);
     imgResize=imresize(imgRead,[16 16]);
+    imgVector = imgResize(:); 
     end
 end
 
