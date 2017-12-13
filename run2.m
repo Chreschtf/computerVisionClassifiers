b189b8b7d49d9df368482293d6347dfb7ffa09c7
Frantzen Christian <frantzenchristian@hotmail.com>
2017-12-13 19:44:10 +0000

    run2 fileprint in progress

---
+++ run2.m
@@ -0,0 +1,39 @@
+setDir  = fullfile('./training');
+imds = imageDatastore(setDir,'IncludeSubfolders',true,'LabelSource','foldernames');
+[trainingSet,testSet] = splitEachLabel(imds,0.3,'randomize');
+%%
+bag = bagOfFeatures(trainingSet,'Gridstep',[4 4],'BlockWidth',[32]);%,'Verbose',false);   
+%%
+categoryClassifier = trainImageCategoryClassifier(trainingSet,bag);
+%%
+confMatrix = evaluate(categoryClassifier,testSet);
+%%
+testDir=fullfile('./testing');
+img = imread(fullfile(testDir,'2.jpg'));
+[labelIdx, score] = predict(categoryClassifier,img);
+categoryClassifier.Labels(labelIdx);
+
+%%
+testDir=fullfile('./testing');
+%exts = {'.jpg','.png','.tif'};
+%testImds = imageDatastore(testDir,'FileExtensions',exts);
+testImages=dir(testDir);
+imagePredictions = strings(length(testImages),2);
+for i=3:length(testImages)
+    %1 and 2  = . and ..
+    testImageName = testImages(i).name;
+    img= imread(fullfile(testDir,testImageName));
+    [labelIdx, score] = predict(categoryClassifier,img);
+    assignedLabel = categoryClassifier.Labels(labelIdx);
+    
+    imagePredictions(i-2,2)=assignedLabel;
+    imagePredictions(i-2,1)=testImageName;
+end
+
+%%
+
+fileID = fopen('run2.txt','w');
+for i=1:length(imagePredictions)
+    fprintf(fileID,'%s %s\n',imagePredictions(i,1),imagePredictions(i,2));
+end
+fclose(fileID);
